5e40e7000caca4ceb93c18fcedc1467e7ea8f1d1
sakib-xrz <sakibxrz21@gmail.com>
2022-10-28 11:48:54 +0600

    new project start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { Toaster } from "react-hot-toast";
-import { RouterProvider } from "react-router-dom";
-import "./App.css";
-import { router } from "./Routes/Routes";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-      <Toaster position="top-center" />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
